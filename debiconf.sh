e89d7650aa7111a17e4e6e1c08c624a930397b21
David96cz <963dasty@seznam.cz>
2026-04-07 17:16:23 +0200

    LXQt updates

--- debiconf.sh
+++ debiconf.sh
@@ -30,7 +30,7 @@ fi
 echo "--------------------------------------------------"
 echo "VOLBA DESKTOPOVÉHO PROSTŘEDÍ"
 echo "1) KDE Plasma"
-echo "2) LXQT (Moderní Lubuntu-style)"
+echo "2) LXQT (Ready out of the box)"
 echo "--------------------------------------------------"
 read -p "Vyber číslo (default 2): " ENV_CHOICE
 
